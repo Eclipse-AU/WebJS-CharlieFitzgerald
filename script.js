fee35951047e7f4448b3716d7bd8c812aef424b5
Charlie <charles.fitzgerald@education.nsw.gov.au>
2023-06-23 14:37:07 +1000

    initial commit
    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+var randomNumber = 0;   // Variables
+var playerGuess = 0;
+var playerScore = 0;
+var computerScore = 0;
+var playerTip = 0;
+var countDownFrom = 10;
+var currentSecond = 11;
+var timerId = 0;
+
+function gameLoop () {
+    generateRandomNumber ();
+    randomNumber = 5; //debug only
+    equalityCheck ();
+    updateHtmlContent ();
+    //secondCountDown();
+}
+
+function generateRandomNumber () {    // Generate random number TEST
+    randomNumber = Math.floor((Math.random() * 10) + 1);
+    // playerScore = randomNumber;
+    // alert (randomNumber) creates an alert of line 19 (playerScore)
+}
+
+function equalityCheck () {     // Equality check TEST
+    playerGuess = document.getElementById("playerGuessInput").value;
+    playerGuess = Number (playerGuess)
+    if (playerGuess === randomNumber) {
+        currentSecond = 11;
+        playerScore = playerScore +1;
+        playerTip = "Correct, click to play again";
+        document.getElementById("playButton").value = "Play now";
+        // clearTimeout (timerId);
+    }
+}
+
+function updateHtmlContent () {     // Updates the HTML page
+    document.getElementById("playerScoreContent").innerHTML = playerScore;
+    document.getElementById("computerScoreContent").innerHTML = computerScore;
+    document.getElementById("tipContent").innerHTML = playerTip;
+}
